35c1f0783129df951d9953ead3255cebfe011718
Alexandre Henrique da Rocha Araujo <64115785+alexandre-henrique-rp@users.noreply.github.com>
2022-01-18 12:23:16 -0300

    update

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ app.post('/send/email', function (req, res) {
     host: process.env.EMAIL_HOST,
     port: process.env.EMAIL_PORT,
     secureConnection: false,
-    requireTLS: true,
+    requireTLS: false,
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASSWORD,
